c660945ec725e65352b1137d145f9f049269abec
odionman <ttkoshy@gmail.com>
2018-10-29 18:52:11 -0400

    wip pong in js and html5

---
+++ pong.js
@@ -0,0 +1,12 @@
+var game, canvas, ctx, soundLeft, soundRight, soundWall, gameTimeLast;
+
+function init() {
+canvas = document.getElementById("gameCanvas");
+ctx = canvas.getContext("2d");
+soundLeft = document.getElementById("bounceLeft");
+soundRight = document.getElementById("bounceRight");
+soundWall = document.getElementById("bounceWall");
+game = {
+
+}
+}
